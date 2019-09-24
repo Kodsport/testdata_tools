1fa678be2e269782207916ed9aaca435d8ab1709
Simon Lindholm <simon.lindholm10@gmail.com>
2019-09-24 18:50:13 +0200

    Reuse sample cases in arithmetic example

--- examples/arithmetic/data/generator.sh
+++ examples/arithmetic/data/generator.sh
@@ -14,6 +14,7 @@ sample_manual 4
 
 group group1 25
 limits ab=10 c=10
+tc 1
 tc tiny-01 gen_random a=1 b=1 c=1
 tc tiny-02 gen_random a=1 b=1 c=10
 tc tiny-03 gen_random a=10 b=10 c=10
@@ -23,6 +24,7 @@ tc tiny-05 gen_random a=10 b=10 c=10
 group group2 25
 limits ab=1e4 c=1e4
 include_group group1
+tc 2
 tc small-01 gen_random a=1 b=1 c=10000
 tc small-02 gen_random a=10000 b=10000 c=1
 tc small-03 gen_random a=10000 b=10000 c=7
@@ -32,6 +34,8 @@ tc small-06 gen_random a=10000 b=10000 c=10000
 
 group group3 25
 limits ab=1e9 c=1
+tc 2
+tc 3
 tc tiny-01
 tc mult-02 gen_random a=1000000000 b=1000000000 c=1
 tc mult-03 gen_random a=1000000000 b=1000000000 c=1
@@ -42,6 +46,7 @@ group group4 25
 limits ab=1e9 c=1e9
 include_group group2
 include_group group3
+tc 4
 tc large-01 gen_random a=1000000000 b=1000000000 c=-1
 tc large-02 gen_random a=-1000000000 b=-1000000000 c=-3
 tc large-03 gen_random a=-1000000000 b=-1000000000 c=-7
