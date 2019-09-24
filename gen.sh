b8ab980aab05f8f28c920aa09d2c2137ef4d8462
Simon Lindholm <simon.lindholm10@gmail.com>
2019-09-24 19:51:47 +0200

    Only use ASAN on Linux
    Closes #9. May reopen later...

--- gen.sh
+++ gen.sh
@@ -95,7 +95,7 @@ add_program cat "bash -c cat<\$0"
 # Arguments: file opts
 compile_cpp () {
   echo Compiling $1...
-  if [[ $2 == *"opt"* ]]; then
+  if [[ $2 == *"opt"* || "$(uname -s)" != Linux* ]]; then
     g++ -O2 -Wall -std=gnu++14 -DGENERATING_TEST_DATA -o $(_base $1) $1
   else
     g++ -O2 -fsanitize=undefined -fsanitize=address -Wall -std=gnu++14 -DGENERATING_TEST_DATA -o $(_base $1) $1
