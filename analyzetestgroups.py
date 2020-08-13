455c454b170e76d30a43d3d54a2e702351c8039d
Thore Husfeldt <thore.husfeldt@gmail.com>
2020-08-13 15:37:57 +0200

    Refactor again, make linter happy
    Separate parsing into its own class, increase
    readability by cute method dispatch logic.
    Better comments, variable names, etc.

--- analyzetestgroups.py
+++ analyzetestgroups.py
@@ -45,7 +45,6 @@ import subprocess
 import argparse
 import itertools
 import logging
-from enum import Enum
 from pathlib import Path
 from collections import defaultdict, OrderedDict
 from typing import List, Optional, Tuple, Dict
@@ -83,61 +82,6 @@ def parse_args() -> argparse.Namespace:
     return argsparser.parse_args()
 
 
-class Pattern(Enum):
-    """Regular expressions needed for parsing the output of
-
-            verifyproblem <problemdir> -l info.
-    """
-
-    END_SUBMISSION = re.compile(
-        r"""
-        (?P<type>\S+)
-        \s+
-        submission
-        \s+
-        (?P<name>\S+)
-        \s+
-        \((?P<language>[^)]+)\)
-        \s+
-        (?P<status>\S+)
-        \s+
-        (?P<grade>\S+)
-        \s+
-        (\((?P<points>\d+)\)\s+)?
-        \[.*CPU:\s(?P<maxtime>\d+.\d+)s.*\]
-        """,
-        re.VERBOSE,
-    )
-    FIRST_LINE = re.compile(r"Loading problem (?P<problemname>\S+)")
-    TESTGROUP_GRADE = re.compile(
-        r"""INFO\ :\ Grade\ on\ test\ case\ group\ data/
-        (?P<type>sample|secret/group)
-        ((?P<number>\d+))?
-        \s+
-        is
-        \s+
-        (?P<grade>\S+)""",
-        re.VERBOSE,
-    )
-    START_SUBMISSION = re.compile(
-        r"INFO : Check (?P<type>\S+) submission (?P<name>\S+)"
-    )
-    START_TESTGROUP = re.compile(
-        r"INFO : Running on test case group data/(sample|secret/group(?P<number>\d+))"
-    )
-    AC_TC_RESULT = re.compile(
-        r"""[T|t]est\ file\ result.*AC.*CPU:\s
-        (?P<time>\d+.\d+)
-        .*
-        test\ case\ (sample|secret/group\d)/
-        (?P<case>[^\]]+)
-        """,
-        re.VERBOSE,
-    )
-    TIMELIMIT = re.compile(
-        r"setting timelim to (?P<limit>\d+) secs, safety margin to (?P<safety>\d+) secs"
-    )
-
 class Verdict:
     """The grader's verdict for a single test group.
 
@@ -148,6 +92,7 @@ class Verdict:
     Attribute time is only non-None for AC grades. Note that time can
     be None even for AC grades (e.g., empty sample input in an interactive problem).
     """
+
     def __init__(self, grade, time=None):
         self.grade = grade
         if grade == "AC":
@@ -184,6 +129,7 @@ class Submission:
             by verifyproblem
         points (int): The total number of points as determined by Verifyproblem.
     """
+
     subdir = {
         "AC": "accepted",
         "PAC": "partially_accepted",
@@ -222,7 +168,7 @@ class Submission:
         """
         return self.expected_total_grade == "AC" or len(self._expected_grades)
 
-    def expected_grade(self, i):
+    def expected_grade(self, i: str):
         """Returns the expected grade on secret group i."""
         if self.expected_total_grade == "AC":
             return "AC"
@@ -231,7 +177,9 @@ class Submission:
     def __init__(self, problempath, expected_total_grade, name):
         self.name = name
         self.expected_total_grade = expected_total_grade
-        self.verdict: Dict[str, Verdict] = OrderedDict() # Note: the type is collections.OrderedDict
+        self.verdict: Dict[
+            str, Verdict
+        ] = OrderedDict()  # Note: the type is collections.OrderedDict
         self.maxtime: Optional[float] = None
         self.points: Optional[int] = None
         path = (
@@ -245,90 +193,179 @@ class Submission:
             logging.warning(
                 "AC submission %s contains EXPECTED_GRADES. "
                 "(Ignored, consider removing it.)",
-                self
+                self,
             )
 
     def __str__(self):
         return self.name
 
 
+class VerificationLogParser:
+    """Parse output from verifyproblem <path> -l info.
+
+    VerificationLogParser.parse() works line by line through given inputstream,
+    matches the current line to various regular expressions in the list
+    VerificationLogParser.pattern and dispatches to class methods accordingly,
+    slowly building a proper Problem object in self.problem.
+
+    Performs various sanity checks on the log to stay in sync.
+    """
+
+    def __init__(self, problem):
+        self.problem: Problem = problem
+        self.tc_times = None  # list of AC times of current test group
+        self.tc_id = None  # current testcase id
+        self.sub = None  # current submission
+        self.lineno = 0
+        self.max_group_id = 0  # largest secret group ID yet seen
+
+    def parse(self, inputstream):
+        """Parse all of inputstream, incrementally building self.problem"""
+        for line in inputstream:
+            self.lineno += 1
+            self.parseline(line)
+            statusline = f"Submission {self.sub}, test case {self.tc_id}"
+            print(" " * 80, end="\r")
+            print(statusline[:80], end="\r")
+
+    def parseline(self, line):
+        """Dispatch the given line among the class methods, based on which
+        VerificationLogParser.pattern matches.
+        """
+        for fun, pattern in VerificationLogParser.patterns.items():
+            match = pattern.search(line)
+            if match:
+                fun(self, match.groupdict())
+
+    def _first_line(self, matchgroup):
+        """Loading problem <problemname>"""
+        self.problem.name = matchgroup["problemname"]
+        if self.problem.name != self.problem.path.stem:
+            sys.exit(
+                f"FATAL: Problem directory does not match log file ({self.problem.name})."
+                "Aborting..."
+            )
+        print(" " * 80, end="\r")
+        print(f"\033[01mAnalyzing problem: {self.problem.name}\033[0m")
+
+    def _start_submission(self, matchgroup):
+        """INFO : Check <type> submission <name>")"""
+        self.sub = Submission(self.problem.path, matchgroup["type"], matchgroup["name"])
+
+    def _start_testgroup(self, _):
+        r"INFO : Running on test case group data/(sample|secret/group<number>)"
+        self.tc_times: List[float] = []
+
+    def _ac_tc_result(self, matchgroup):
+        """Test file result ... AC ... <time> ... test case ... <case>"""
+        print(self.problem.name, self.sub, end="\r")
+        self.tc_times.append(float(matchgroup["time"]))
+        self.tc_id = matchgroup["case"]
+
+    def _testgroup_grade(self, matchgroup):
+        """INFO : Grade on test case group ... <type> ... <number> is <grade>"""
+        assert self.sub is not None
+        grade = matchgroup["grade"]
+        time = max(self.tc_times) if len(self.tc_times) > 0 else None
+        if matchgroup["type"] == "sample":
+            self.sub.verdict["sample"] = Verdict(grade, time)
+        else:
+            i = matchgroup["number"]
+            self.sub.verdict[i] = Verdict(grade, time)
+            if grade == "AC" and time is None:
+                logging.error(
+                    "Line %d of verifyproblem: "
+                    "AC grade for secret group %d requires at least one test case",
+                    self.lineno,
+                    i,
+                )
+            self.max_group_id = max(int(i), self.max_group_id)
+
+    def _end_submission(self, matchgroup):
+        """<type> submission <name> (<language>) (<status>) (<grade>) <points> ...CPU: <maxtime>"""
+        assert self.sub is not None
+        self.sub.points = int(matchgroup["points"] or "0")
+        self.sub.maxtime = float(matchgroup["maxtime"])
+        self.problem.submissions.append(self.sub)
+
+    def _timelimit(self, matchgroup):
+        """setting timelim to <limit> secs, safety margin to <safety> secs"""
+        self.problem.timelimits = int(matchgroup["limit"]), int(matchgroup["safety"])
+
+    patterns = {
+        _first_line: re.compile(r"Loading problem (?P<problemname>\S+)"),
+        _testgroup_grade: re.compile(
+            r"""INFO\ :\ Grade\ on\ test\ case\ group\ data/
+        (?P<type>sample|secret/group)
+        ((?P<number>\d+))?
+        \s+ is \s+
+        (?P<grade>\S+)""",
+            re.VERBOSE,
+        ),
+        _start_submission: re.compile(
+            r"INFO : Check (?P<type>\S+) submission (?P<name>\S+)"
+        ),
+        _start_testgroup: re.compile(
+            r"INFO : Running on test case group data/(sample|secret/group(?P<number>\d+))"
+        ),
+        _ac_tc_result: re.compile(
+            r"""[T|t]est\ file\ result.*AC.*CPU:\s
+        (?P<time>\d+.\d+)
+        .* test\ case\ (sample|secret/group\d)/
+        (?P<case>[^\]]+)
+        """,
+            re.VERBOSE,
+        ),
+        _timelimit: re.compile(
+            r"setting timelim to (?P<limit>\d+) secs, safety margin to (?P<safety>\d+) secs"
+        ),
+        _end_submission: re.compile(
+            r"""
+        (?P<type>\S+)
+        \s+ submission \s+
+        (?P<name>\S+)
+        \s+
+        \((?P<language>[^)]+)\)
+        \s+
+        (?P<status>\S+)
+        \s+
+        (?P<grade>\S+)
+        \s+
+        (\((?P<points>\d+)\)\s+)?
+        \[.*CPU:\s(?P<maxtime>\d+.\d+)s.*\]
+        """,
+            re.VERBOSE,
+        ),
+    }
+
+
 class Problem:
     """A problem.
 
     Attributes:
+        name (str): the problem's name, say "hello"
+        path (Path): the problem path, say Path("../kattis_stuff/hello")
         submissions (List[Submissions]): the submissions making up this problem
-        groups (List[str]): the secret test groups, "1", "2", ...
+        groups (List[str]): the secret test groups, ["1", "2", ...]
         timelimits (Tuple[int, int]): the timelimit and safe timelimit determined
             by verifyproblem
     """
 
-    @staticmethod
-    def _find_matching_pattern(line):
-        """Returns a match object that matches the given line, or None"""
-        for pat in Pattern:
-            match = pat.value.search(line)
-            if match:
-                return pat, match
-        return None, None
-
     def __init__(self, problempath, inputstream):
+        self.path = problempath
         self.submissions: List[Submission] = []
+        self.timelimits = None, None
 
-        sub = tc_id = None
-        lineno = max_group_id = 0
-        for line in inputstream:
-            lineno += 1
-            pattern, match = Problem._find_matching_pattern(line)
-            if match is None:
-                continue
-            d = match.groupdict()
-            if pattern == Pattern.FIRST_LINE:
-                problemname = d["problemname"]
-                if problemname != problempath.stem:
-                    sys.exit(
-                        f"FATAL: Problem directory does not match log file ({problemname})."
-                        "Aborting..."
-                    )
-                print(" " * 80, end="\r")
-                print(f"\033[01mAnalyzing problem: {problemname}\033[0m")
-            elif pattern == Pattern.START_SUBMISSION:
-                sub = Submission(problempath, d["type"], d["name"])
-            elif pattern == Pattern.START_TESTGROUP:
-                tc_times: List[float] = []
-            elif pattern == Pattern.AC_TC_RESULT:
-                print(problemname, sub, end="\r")
-                tc_times.append(float(d["time"]))
-                tc_id = d["case"]
-            elif pattern == Pattern.TESTGROUP_GRADE:
-                assert sub is not None
-                sub.verdict["sample" if d["type"] == "sample" else d["number"]] = Verdict(
-                    d["grade"], max(tc_times) if len(tc_times) > 0 else None
-                )
-                if d["type"] != "sample":
-                    if d["grade"] == "AC" and len(tc_times) == 0:
-                        logging.error(
-                            "Line %d of verifyproblem: "
-                            "AC grade for secret group requires at least one test case",
-                            lineno
-                        )
-                    max_group_id = max(int(d["number"]), max_group_id)
-            elif pattern == Pattern.END_SUBMISSION:
-                assert sub is not None
-                sub.points = int(d["points"] or "0")
-                sub.maxtime = float(d["maxtime"])
-                self.submissions.append(sub)
-            elif pattern == Pattern.TIMELIMIT:
-                self.timelimits = int(d["limit"]), int(d["safety"])
-            statusline = f"Submission {sub}, test case {tc_id}"
-            print(" " * 80, end="\r")
-            print(statusline[:80], end="\r")
+        parser = VerificationLogParser(self)
+        parser.parse(inputstream)
 
-        self.groups = list(str(i) for i in range(1, max_group_id + 1))
+        # sanity check: make sure every submission has verdicts for
+        # "sample", "1", "2", ... and that the number of groups is consistent
+        self.groups = list(str(i) for i in range(1, parser.max_group_id + 1))
         allgroups = ["sample"] + self.groups
         for sub in self.submissions:
-            if list(sub.verdict.keys()) != allgroups:  # Note: this is order-sensitive
+            if list(sub.verdict.keys()) != allgroups:  # Note: verdict is ordered dict
                 logging.error("Unexpected group name for submission %s.", sub)
-
 
     def print_table(self):
         """Print a table of verdicts for each submission, and possibly emit
@@ -341,14 +378,14 @@ class Problem:
         print(f"{'Submission':{alignto}} Sample  ", end=" ")
         print(" ".join(f"Group {i} " for i in self.groups), end=" ")
         print("Pts Time  Expected\033[0m")
-        for sub in self.submissions:
+        for sub in sorted(self.submissions, key=lambda d: (-d.points, d.maxtime)):
             print(f"{sub.name:{alignto}}", end=" ")
             for verdict in sub.verdict.values():
                 print(f"{verdict:17}", end=" ")
             print(f"{sub.points:3}", end=" ")
             print(f"{sub.maxtime:4.2f}s", end=" ")
 
-            if sub.has_expected_grades:
+            if sub.has_expected_grades():
                 summary = []
                 for i in self.groups:
                     if sub.expected_grade(i) == sub.verdict[i].grade:
@@ -364,16 +401,18 @@ class Problem:
         for sub, warngroups in warnings.items():
             for i in warngroups:
                 logging.warning(
-                    "%s: Unexpected grade %s on test group %s. "
-                    "(Expected %s).",
-                    sub, sub.verdict[i].grade, i, sub.expected_grade(i)
+                    "%s: Unexpected grade %s on test group %s. " "(Expected %s).",
+                    sub,
+                    sub.verdict[i].grade,
+                    i,
+                    sub.expected_grade(i),
                 )
         for sub, expectations in suggestions:
             logging.info(
                 "%s: No hint found. Consider adding '@EXPECTED_GRADES@ %s'.",
-                sub, expectations
+                sub,
+                expectations,
             )
-
 
     def check_distinguished(self):
         """Check if all secrete test groups are distinguished by some submission.
@@ -389,7 +428,9 @@ class Problem:
             if accepting_subs[i] == accepting_subs[j]:
                 logging.warning(
                     "No submission distinguishes test groups %s and %s. "
-                    "Consider adding one, or merging groups.", i, j
+                    "Consider adding one, or merging groups.",
+                    i,
+                    j,
                 )
             all_distinguished = False
         if all_distinguished:
@@ -413,7 +454,9 @@ def main():
     with open(Path(args.problemdir) / "problem.yaml", encoding="utf-8") as file:
         problemtype = yaml.safe_load(file).get("type")
         if problemtype != "scoring":
-            logging.critical("%s is not a scoring problem. Aborting...", args.problemdir)
+            logging.critical(
+                "%s is not a scoring problem. Aborting...", args.problemdir
+            )
             sys.exit(1)
     if not args.logfile:
         verifyproblem = subprocess.Popen(
@@ -430,9 +473,6 @@ def main():
     problempath = Path(args.problemdir).resolve()
 
     problem = Problem(problempath, inputstream)
-
-    problem.submissions.sort(key=lambda d: (-d.points, d.maxtime))
-
     problem.print_table()
     print(f"Time limit: {problem.timelimits[0]}s, safe: {problem.timelimits[1]}s")
     problem.check_distinguished()
